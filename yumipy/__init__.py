3c4580ee7a2b48da5a3a5fd3456d1bcff8161f2f
BinaryHalibut <alanpusongli@gmail.com>
2017-03-10 00:14:42 -0800

    add YuMiArmFactory to package __init__

--- yumipy/__init__.py
+++ yumipy/__init__.py
@@ -6,14 +6,14 @@ from yumi_constants import YuMiConstants
 from yumi_state import YuMiState
 from yumi_trajectory import YuMiTrajectory
 from yumi_planner import YuMiMotionPlanner
-from yumi_arm import YuMiArm
+from yumi_arm import YuMiArm, YuMiArmFactory
 from yumi_robot import YuMiRobot
 from reset_device import ResetDevice
 from yumi_motion_logger import YuMiMotionLogger
 from yumi_subscriber import YuMiSubscriber
 from yumi_exceptions import YuMiCommException, YuMiControlException
 
-__all__ = ['YuMiConstants', 'YuMiState', 'YuMiArm', 'YuMiRobot', 'ResetDevice',
+__all__ = ['YuMiConstants', 'YuMiState', 'YuMiArm', 'YuMiArmFactory', 'YuMiRobot', 'ResetDevice',
 			'YuMiTrajectory', 'YuMiMotionPlanner',
 			'YuMiMotionLogger', 'YuMiCommException', 'YuMiControlException',
             'YuMiSubscriber',
